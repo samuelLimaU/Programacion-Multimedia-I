40fa6b483eb5a955e04921202e620b6eb09031f0
unknown <lpze.samuelignacio.lima.es@unifranz.edu.bo>
2025-08-14 22:54:37 -0400

    primer commit

---
+++ Diagnostico_Multimedia_SAMUEL_LIMA_E/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Diagnostico_Multimedia_SAMUEL_LIMA_E
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.int_inicio = new System.Windows.Forms.TextBox();
+            this.int_fin = new System.Windows.Forms.TextBox();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.btn_aceptar = new System.Windows.Forms.Button();
+            this.btn_borrar = new System.Windows.Forms.Button();
+            this.lbl_3 = new System.Windows.Forms.Label();
+            this.lbl_numeros = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_tiempo = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // int_inicio
+            // 
+            this.int_inicio.Location = new System.Drawing.Point(23, 145);
+            this.int_inicio.Name = "int_inicio";
+            this.int_inicio.Size = new System.Drawing.Size(100, 26);
+            this.int_inicio.TabIndex = 0;
+            // 
+            // int_fin
+            // 
+            this.int_fin.Location = new System.Drawing.Point(176, 145);
+            this.int_fin.Name = "int_fin";
+            this.int_fin.Size = new System.Drawing.Size(100, 26);
+            this.int_fin.TabIndex = 1;
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Location = new System.Drawing.Point(19, 122);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(46, 20);
+            this.lbl_1.TabIndex = 2;
+            this.lbl_1.Text = "Inicio";
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Location = new System.Drawing.Point(172, 122);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(31, 20);
+            this.lbl_2.TabIndex = 3;
+            this.lbl_2.Text = "Fin";
+            // 
+            // btn_aceptar
+            // 
+            this.btn_aceptar.Location = new System.Drawing.Point(176, 216);
+            this.btn_aceptar.Name = "btn_aceptar";
+            this.btn_aceptar.Size = new System.Drawing.Size(80, 35);
+            this.btn_aceptar.TabIndex = 4;
+            this.btn_aceptar.Text = "Aceptar";
+            this.btn_aceptar.UseVisualStyleBackColor = true;
+            this.btn_aceptar.Click += new System.EventHandler(this.btn_aceptar_Click);
+            // 
+            // btn_borrar
+            // 
+            this.btn_borrar.Location = new System.Drawing.Point(43, 216);
+            this.btn_borrar.Name = "btn_borrar";
+            this.btn_borrar.Size = new System.Drawing.Size(80, 35);
+            this.btn_borrar.TabIndex = 5;
+            this.btn_borrar.Text = "Borrar";
+            this.btn_borrar.UseVisualStyleBackColor = true;
+            this.btn_borrar.Click += new System.EventHandler(this.btn_borrar_Click);
+            // 
+            // lbl_3
+            // 
+            this.lbl_3.AutoSize = true;
+            this.lbl_3.Location = new System.Drawing.Point(19, 287);
+            this.lbl_3.Name = "lbl_3";
+            this.lbl_3.Size = new System.Drawing.Size(205, 20);
+            this.lbl_3.TabIndex = 6;
+            this.lbl_3.Text = "La cantidad de números es:";
+            // 
+            // lbl_numeros
+            // 
+            this.lbl_numeros.AutoSize = true;
+            this.lbl_numeros.Location = new System.Drawing.Point(230, 287);
+            this.lbl_numeros.Name = "lbl_numeros";
+            this.lbl_numeros.Size = new System.Drawing.Size(0, 20);
+            this.lbl_numeros.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 319);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Tiempo de ejecución:";
+            // 
+            // lbl_tiempo
+            // 
+            this.lbl_tiempo.AutoSize = true;
+            this.lbl_tiempo.Location = new System.Drawing.Point(183, 319);
+            this.lbl_tiempo.Name = "lbl_tiempo";
+            this.lbl_tiempo.Size = new System.Drawing.Size(0, 20);
+            this.lbl_tiempo.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 80);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Alto consejo, ya realice la forma de \r\ndepurar a esos malditos compuestos, \r\nsolo" +
+    " escriba numero entero \r\nde inicio y de final\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 354);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbl_tiempo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_numeros);
+            this.Controls.Add(this.lbl_3);
+            this.Controls.Add(this.btn_borrar);
+            this.Controls.Add(this.btn_aceptar);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.lbl_1);
+            this.Controls.Add(this.int_fin);
+            this.Controls.Add(this.int_inicio);
+            this.Name = "Form1";
+            this.Text = "Identificador de Primos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox int_inicio;
+        private System.Windows.Forms.TextBox int_fin;
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Button btn_aceptar;
+        private System.Windows.Forms.Button btn_borrar;
+        private System.Windows.Forms.Label lbl_3;
+        private System.Windows.Forms.Label lbl_numeros;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_tiempo;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Diagnostico_Multimedia_SAMUEL_LIMA_E/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diagnostico_Multimedia_SAMUEL_LIMA_E
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // funcion que genera todos los primos hasta un limite
+        // usando la criba de erostenes
+        static List<int> GenerarPrimosBase(int limite)
+        {
+            // true es primo, false no lo es
+            bool[] esPrimo = new bool[limite + 1];
+
+            // todos los numeros ≥ 2 como posibles primos
+            for (int i = 2; i <= limite; i++)
+                esPrimo[i] = true;
+
+            // aplicar la criba de 
+            // recorrer cada numero p desde 2 hasta raiz 2 del limtie
+            for (int p = 2; p * p <= limite; p++)
+            {
+                // si p esta como primo
+                if (esPrimo[p])
+                {
+                    // se marca todos los multiplos de p a partir de p*p como no primos
+                    for (int m = p * p; m <= limite; m += p)
+                        esPrimo[m] = false;
+                }
+            }
+
+            // lista para almacenar los primos encontrados
+            List<int> primos = new List<int>();
+            for (int i = 2; i <= limite; i++)
+                if (esPrimo[i])
+                    primos.Add(i);
+
+            return primos;
+            //se devuelve la lista con los primos
+        }
+
+        // criba segmentada para contar primos
+        static long ContarPrimosSegmentado(long L, long R)
+        {
+            // limite superior es menor que 2, no hay primos en el rango
+            if (R < 2) return 0;
+
+            // limite inferior es menor que 2, lo ajustamos a 2
+            // porque 1 no se considera primo
+            if (L < 2) L = 2;
+
+            // tamano del bloque
+            // bloques grandes = mas memoria pero menos iteraciones
+            int bloque = 500_000_000;
+            long contador = 0; // Contador de primos encontrados
+
+            // obtener los primos base hasta raiz 2 de 2 usando la criba simple
+            int limiteBase = (int)Math.Floor(Math.Sqrt(R));
+            List<int> primosBase = GenerarPrimosBase(limiteBase);
+
+            // preocesar el rango L R en bloques
+            for (long low = L; low <= R; low += bloque)
+            {
+                // define el limite superior del bloque actual
+                long high = Math.Min(low + bloque - 1, R);
+
+                // array booleano indica si un numero dentro del bloque es primo
+                bool[] esPrimoSegmento = new bool[high - low + 1];
+
+                for (int i = 0; i < esPrimoSegmento.Length; i++)
+                    esPrimoSegmento[i] = true;
+
+                // marca multiplos de cada primo base en el bloque actual
+                foreach (int p in primosBase)
+                {
+                    // calcula el primer multiplo de p dentro del bloque
+                    long inicio = Math.Max((long)p * p, ((low + p - 1) / p) * p);
+
+                    // marca todos los multiplos de p como no primos
+                    for (long m = inicio; m <= high; m += p)
+                        esPrimoSegmento[m - low] = false;
+                }
+
+                // contar los primos en el bloque
+                for (long i = low; i <= high; i++)
+                    if (esPrimoSegmento[i - low])
+                        contador++;
+            }
+
+            // devolvert el total de primos encontrados en todo el rango
+            return contador;
+        }
+
+
+        private async void btn_aceptar_Click(object sender, EventArgs e)
+        {
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start(); // inicia el cronometro
+
+            if (!long.TryParse(int_inicio.Text, out long L) ||
+                !long.TryParse(int_fin.Text, out long R))
+            {
+                MessageBox.Show("Introduce valores numéricos válidos.");
+                return;
+            }
+
+            if (L == 0 && R == 0)
+            {
+                lbl_numeros.Text = "Chaudo";
+                return;
+            }
+
+            lbl_numeros.Text = "Calculando...";
+
+            // ejecuta en segundo plano para no congelar la UI
+            long cantidad = await Task.Run(() => ContarPrimosSegmentado(L, R));
+
+            sw.Stop(); // Detiene el cronometro
+
+            lbl_tiempo.Text = $"La ejecución duró {sw.ElapsedMilliseconds} ms";
+
+            lbl_numeros.Text = cantidad.ToString();
+        }
+
+        private void btn_borrar_Click(object sender, EventArgs e)
+        {
+            int_inicio.Text = "";
+            int_fin.Text = "";
+            lbl_numeros.Text = "";
+            //borrar las entradas y salidas
+        }
+    }
+}
